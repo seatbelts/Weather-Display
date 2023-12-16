f589f15c29067997afd2a13117984132b0f7b89a
seatbelts <is.ma95@hotmail.com>
2023-12-15 21:12:50 -0600

    Added Functionality and content to WeatherDetails

--- src/app/weather/weather-list/weather-list.component.ts
+++ src/app/weather/weather-list/weather-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router'
 
 @Component({
   selector: 'app-weather-list',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class WeatherListComponent {
 
+  forecasts = [
+    {
+      title: 'District of Columbia',
+      abbreviation: 'LOX',
+      id: 'lox'
+    },
+    {
+      title: 'Kansas',
+      abbreviation: 'TOP',
+      id: 'top'
+    }
+  ];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+
+  navigateTo(forecastId: string): void {
+    this.router.navigate([forecastId], { relativeTo: this.route });
+  }
 }
