22b48e49bbcd88810e2d7d4c3bc3533c262b11fc
seatbelts <is.ma95@hotmail.com>
2023-12-16 10:58:53 -0600

    Added NgCharts module to proper feature Module

--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+
+import { NgChartsModule } from 'ng2-charts';
+
 import { WeatherListComponent } from './weather-list/weather-list.component';
 import { WeatherDetailsComponent } from './weather-details/weather-details.component';
 import { WeatherRoutingModule } from './weather-routing.module';
+
 import { WeatherService } from './services/weather.service';
 
 
@@ -16,6 +20,7 @@ import { WeatherService } from './services/weather.service';
   imports: [
     CommonModule,
     HttpClientModule,
+    NgChartsModule,
     WeatherRoutingModule
   ],
   providers: [
